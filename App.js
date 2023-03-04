8dd7708080440b3b6d1bb88295e9a94c88cb1cb4
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-03-04 11:23:54 +0500

    Smart Pearl Solution webview added after splash screen

--- App.js
+++ App.js
@@ -2,40 +2,40 @@ import { View, Image, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WebView } from 'react-native-webview';
+
 
 function SplashScreen({ navigation }) {
-  setTimeout(() => { navigation.navigate('NextScreen') }, 500);
+  setTimeout(() => { navigation.navigate('NextScreen') }, 5000);
   return (
     <>
       <View style={{ flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
-        < Image  source={require('./src/assets/img1.jpg')} style={{ height: 100, width: 100, borderRadius: 100 }} />
+        < Image source={require('./src/assets/img1.jpg')} style={{ height: 100, width: 100, borderRadius: 100 }} />
       </View>
-    </> 
+    </>
   )
 }
 
 function NextScreen({ navigation }) {
   return (
-    <>
-     <View style={{ flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Heloo World</Text>
-      </View>
-    
-    </>
+    <WebView source={{ uri: 'https://smartpearlsolutions.com/' }} />
   )
 }
 
 const Stack = createNativeStackNavigator();
+
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="NextScreen" component={NextScreen} options={{
-          headerShown: false
-        }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="NextScreen" component={NextScreen} options={{
+            headerShown: false
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   )
 }
 export default App;
